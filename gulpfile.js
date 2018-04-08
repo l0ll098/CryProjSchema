71c5613076f28a9840518615d95021f15bf7a177
Lorenzo Montanari <lorenzo0.montanari@gmail.com>
2018-04-08 18:13:08 +0200

    Show an error message if the console dump file is not found

--- gulpfile.js
+++ gulpfile.js
@@ -5,7 +5,8 @@ const fs = require("fs");
 const jsonBeautifier = require("./jsonBeautifier");
 
 let tasks = Object.freeze({
-    generateSchema: "generateSchema"
+    generateSchema: "generateSchema",
+    generateSchemaForAllVersions: "generateSchemaForAllVersions"
 });
 
 let placeholders = Object.freeze({
@@ -14,19 +15,12 @@ let placeholders = Object.freeze({
     commands: "$$$2$$$"
 });
 
-gulp.task(tasks.generateSchema, () => {
-    let version = 54
-
-    return gulp
-        .src("cryproj.partial.schema.json")
-        .pipe(replace(placeholders.version, version.toString()[0] + "." + version.toString()[1]))
-        .pipe(replace(placeholders.cvars, fetchCvars(version)))
-        .pipe(replace(placeholders.commands, fetchCommands(version)))
-        .pipe(rename("cryproj." + version + ".schema.json"))
-        .pipe(jsonBeautifier("\t"))
-        .pipe(gulp.dest("out/"));
-});
-
+let engineVersions = [
+    "52",
+    "53",
+    "54",
+    "5dev"
+];
 
 function fetchCvars(version) {
     let content = fs.readFileSync("./in/consolecommandsandvars." + version + ".txt", "utf-8");
@@ -60,7 +54,41 @@ function fetchCommands(version) {
     return commands.join("\",\"");
 }
 
+function generateSchema(version) {
+    let engineVersion = version.toString()[0] + "." + version.substring(1, version.length);
+
+    if (fs.existsSync("./in/consolecommandsandvars." + version + ".txt")) {
+        return gulp
+            .src("cryproj.partial.schema.json")
+            .pipe(replace(placeholders.version, engineVersion))
+            .pipe(replace(placeholders.cvars, fetchCvars(version)))
+            .pipe(replace(placeholders.commands, fetchCommands(version)))
+            .pipe(rename("cryproj." + version + ".schema.json"))
+            .pipe(jsonBeautifier("\t"))
+            .pipe(gulp.dest("out/"));
+    } else {
+        console.log("Skipping generation of the schema file for engine version " + engineVersion + " as the file containing the commands and cvars has not been found");
+    }
+}
+
+/**
+ * Generate the schema file for the dev engine
+ */
+gulp.task(tasks.generateSchema, () => {
+    return generateSchema(engineVersions[engineVersions.length - 1]);
+});
+
+/**
+ * Generate the schema file for every engine version.
+ * If the commands and cvars dump file is not found, a message will be shown
+ */
+gulp.task(tasks.generateSchemaForAllVersions, () => {
+    engineVersions.forEach((version) => {
+        generateSchema(version);
+    });
+});
+
 /**
  * Run all
  */
-gulp.task("default", [tasks.generateSchema]);
+gulp.task("default", [tasks.generateSchemaForAllVersions]);
